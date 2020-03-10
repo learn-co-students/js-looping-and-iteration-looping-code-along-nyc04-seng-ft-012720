03c59c2dbfadac4105b6306a17381e8a855e7f60
abidtalukdar <abiditalukdar@gmail.com>
2020-03-10 16:55:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let arr = [];
+    for ( let i = 0; i < names.length; i++ ) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return arr;
+}
+
+function countDown(positiveNum) {
+    while (positiveNum >= 0) {
+        console.log(positiveNum);
+        positiveNum--;
+    }
+}
